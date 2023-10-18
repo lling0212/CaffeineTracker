913f969b3d29a2cd0186c2fd0b455f384ac4d385
Lisa Ling <lisaling.yx@gmail.com>
2023-10-18 18:56:50 -0400

    Initial Commit

---
+++ CaffeineTracker/CaffeineTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffeineTrackerApp.swift
+//  CaffeineTracker
+//
+//  Created by Lisa's Mac Pro on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct CaffeineTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaffeineTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaffeineTracker
+//
+//  Created by Lisa's Mac Pro on 2023/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
